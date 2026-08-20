2c456bd71b6d8b84d3b699598495eecbdd10aaf1
Richard Nguyen <rich.nguyen20941@gmail.com>
2026-08-19 23:45:29 -0700

    Added cache read/write hit and miss logic

--- cache_memory/cache.cpp
+++ cache_memory/cache.cpp
@@ -1,6 +1,8 @@
 #include "cache.hpp"
 #include <iostream>
 
+/* cache implementation */
+
 Cache::Cache(Memory& mem) : memory(mem) {
     cacheMemory.resize(16);
 }
@@ -15,10 +17,16 @@ uint8_t Cache::cacheRead(const uint16_t address){
     if (line.valid && line.tag == tag){
         //cache hit
         return line.data[offset];
-    } else {
-        //cache miss
-        return memory.read(address);
+    } 
+
+    uint16_t blockStart = address & 0xFFF0;
+    for (int i = 0; i < 16; i++){
+        line.data[i] = memory.read(blockStart + i);
     }
+    line.valid = true;
+    line.tag = tag;
+
+    return line.data[offset];
 }
 
 void Cache::cacheWrite(const uint16_t address, uint8_t val){
@@ -29,10 +37,11 @@ void Cache::cacheWrite(const uint16_t address, uint8_t val){
     CacheLine& line = cacheMemory[index];
 
     if (line.valid && line.tag == tag){
-        //cache hit
+        //cache hit - currently write through 
         line.data[offset] = val;
+        memory.write(address, val);
     } else {
         //cache miss
-        std::cout<<"Invalid write location";
+        memory.write(address, val);
     }
 }
